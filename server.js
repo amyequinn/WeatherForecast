845c2921384a65e668a38c5ca7cf75c72cd592b8
Amy Quinn <amyequinn@hotmail.co.uk>
2020-10-24 20:07:19 +0100

    update

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const path = require('path');
-// const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 const app = express();
 
 Accept-Encoding: gzip, compress, br;
 
 
-var port_number = server.listen(process.env.PORT || 3000);
-app.listen(port_number);
+// var port_number = server.listen(process.env.PORT || 3000);
+// app.listen(port_number);
 
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname + '/dist'));
@@ -15,7 +15,7 @@ app.use(express.static(__dirname + '/dist'));
 // send the user to index html page inspite of the url
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'dist/index.html'));
-  console.log(__dirname)
+
 });
 
-// app.listen(port);
+app.listen(port);
