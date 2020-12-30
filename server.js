4f0d388f9adef067cc39f2a78d7d96ebcb1005a0
Amy Quinn <amyequinn@hotmail.co.uk>
2020-12-30 18:19:10 +0000

    added text compression to server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const compression = require('compression');
 const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
@@ -6,6 +7,8 @@ const app = express();
 // Accept-Encoding: gzip, compress, br;
 
 // the __dirname is the current directory from where the script is running
+app.use(compression());
+
 app.use(express.static(__dirname + '/dist'));
 
 // send the user to index html page inspite of the url
@@ -15,19 +18,3 @@ app.get('*', (req, res) => {
 });
 
 app.listen(port, () => console.log(`Server started at ${port}!`));
-
-
-// const express = require('express');
-// const path = require('path');
-// const port = process.env.PORT || 8080;
-// const app = express();
-//
-// // the __dirname is the current directory from where the script is running
-// app.use(express.static(__dirname + '/dist'));
-//
-// // send the user to index html page inspite of the url
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, 'index.html'));
-// });
-//
-// app.listen(port);
